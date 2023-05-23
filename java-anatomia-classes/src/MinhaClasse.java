3aa2f73b94f7c02b72fa13c14427e82f682ca1e8
Eduardocr80 <ecrcordeirodev@hotmail.com>
2023-05-23 17:51:37 -0300

    Criação de variável e Método

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,17 +1,24 @@
 public class MinhaClasse {
     public static void main(String[] args) {
+
+
+        String primeiroNome = "Eduardo";
+        String segundoNome = "Cordeiro";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+    }
        
-        String meuNome = "Eduardo"; // tipo(string),(NomeBemDefinido "meuNome") = Atribuição (Eduardo)
-        int anoFabricacao = 2022; //tipo(int),(NomeBemDefinido "anoFabricacao") = Atribuição (2022)
+
         
-        boolean simNao = true; // tipo(boolean),(nomeBemDefinido "simNao") = Atribuição (true)
-        boolean verdadeira = true;// tipo(boolean),(nomeBemDefinido "verdadeiro") = Atribuição(true)
-        boolean verdadeira = false;// tipo(boolean),(nomeBemDefinido "verdadeiro") = Atribuição(false)
-        boolean falsa = true;// tipo(boolean),(nomeBemDefinido "falsa") = Atribuição(false)
-        boolean falsa = false;// tipo(boolean),(nomeBemDefinido "falsa") = Atribuição(true)
-        
-        
+        public static String nomeCompeto (String primeiroNome, String segundoNome) {
+            return "Resultado do método" + primeiroNome.concat("").concat(segundoNome);
+
+
+
+    }
+
 
     }
     
-}
+
