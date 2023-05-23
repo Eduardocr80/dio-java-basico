38eda5d9a1b35f9689959b80cc1ef26706b495b5
Eduardocr80 <ecrcordeirodev@hotmail.com>
2023-05-23 16:22:43 -0300

    Modelo de Declaração de Variáveis

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,16 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print("Olá,Java");
+       
+        String meuNome = "Eduardo"; // tipo(string),(NomeBemDefinido "meuNome") = Atribuição (Eduardo)
+        int anoFabricacao = 2022; //tipo(int),(NomeBemDefinido "anoFabricacao") = Atribuição (2022)
         
+        boolean simNao = true; // tipo(boolean),(nomeBemDefinido "simNao") = Atribuição (true)
+        boolean verdadeira = true;// tipo(boolean),(nomeBemDefinido "verdadeiro") = Atribuição(true)
+        boolean verdadeira = false;// tipo(boolean),(nomeBemDefinido "verdadeiro") = Atribuição(false)
+        boolean falsa = true;// tipo(boolean),(nomeBemDefinido "falsa") = Atribuição(false)
+        boolean falsa = false;// tipo(boolean),(nomeBemDefinido "falsa") = Atribuição(true)
+        
+
     }
     
 }
