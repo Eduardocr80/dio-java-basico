36e5d0dda88a927766797a609ad920f0d6155a19
Eduardocr80 <ecrcordeirodev@hotmail.com>
2023-05-23 17:52:17 -0300

    Criação de Variáveis e Métodos

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,24 @@
 public class MinhaClasse {
     public static void main(String[] args) {
 
+        //Criação de Variáveis
+       
+        String meuNome = "Eduardo"; // tipo(string),(NomeBemDefinido "meuNome") = Atribuição (Eduardo)
+        int anoFabricacao = 2022; //tipo(int),(NomeBemDefinido "anoFabricacao") = Atribuição (2022)
+        
+        boolean simNao = true; // tipo(boolean),(nomeBemDefinido "simNao") = Atribuição (true)
+        boolean verdadeira = true;// tipo(boolean),(nomeBemDefinido "verdadeiro") = Atribuição(true)
+        boolean verdadeira = false;// tipo(boolean),(nomeBemDefinido "verdadeiro") = Atribuição(false)
+        boolean falsa = true;// tipo(boolean),(nomeBemDefinido "falsa") = Atribuição(false)
+        boolean falsa = false;// tipo(boolean),(nomeBemDefinido "falsa") = Atribuição(true)
+        
+        // Criação de Métodos
+        
+        //TipoRetorno NomeObjetivoNoInfinitivo Parametro(s)
+
+        int somar (int numeroUm, int numero2) // TipoRetorno(int), NomeObjetivoNoInfinitivo(somar), Parametro(int numeroum, int numero2)
+        
+        String formatarCep (long cep) // TipoRetorno(Strind), NomeObjetivoNoInfinitivo(Formatar), Parametro(long cep)
 
         String primeiroNome = "Eduardo";
         String segundoNome = "Cordeiro";
